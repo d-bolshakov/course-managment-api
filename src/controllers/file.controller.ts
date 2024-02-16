1ab9bdca03c1d342d21568bab47b763367a4b6cb
Denys Bolshakov <deni.bolshakov.ua@gmail.com>
2024-02-16 21:15:44 +0200

    file controller error handling fix

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -1,6 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
 import { inject, injectable } from "tsyringe";
 import type { IFileService } from "../interfaces/services/file-service.interface";
+import createError from "http-errors";
 
 @injectable()
 export class FileController {
@@ -10,9 +11,16 @@ export class FileController {
     res: Response,
     next: NextFunction
   ) {
-    const file = await this.fileService.getById(fileId);
-    res.setHeader("content-type", file.mimetype);
-    if (query.download) res.attachment(file.filename);
-    file.stream.pipe(res);
+    try {
+      const file = await this.fileService.getById(fileId);
+      res.setHeader("content-type", file.mimetype);
+      if (query.download) res.attachment(file.filename);
+      file.stream.pipe(res);
+    } catch (err) {
+      console.error(err);
+      if (err.code === "ENOENT")
+        next(createError.NotFound(`File with id ${fileId} does not exist`));
+      next(err);
+    }
   }
 }
