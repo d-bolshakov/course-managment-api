f445bb38f0e7f122879ae241eb40dbfc9d3decb1
Denys Bolshakov <deni.bolshakov.ua@gmail.com>
2024-02-18 12:42:56 +0200

    added user access strategy and user routes protection

---
+++ src/middleware/access-strategies/user.access-strategy.ts
@@ -0,0 +1,14 @@
+import type { AccessStrategy } from "./access-strategy.js";
+import { UserDto } from "../../dto/user/user.dto.js";
+import { Role } from "../../entities/User.entity.js";
+
+export class UserAccessStrategy implements AccessStrategy {
+  async hasAccess(
+    user: UserDto,
+    resourse: { userId: number }
+  ): Promise<boolean> {
+    if (user.role === Role.ADMIN) return true
+    return user.id === resourse.userId
+    }
+  }
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -8,6 +8,8 @@ import { MeMiddleware } from "../middleware/me.middleware.js";
 import { RoleMiddleware } from "../middleware/role.middleware.js";
 import { Role } from "../entities/User.entity.js";
 import { FilterUserDto } from "../dto/user/filter-user.dto.js";
+import { AccessMiddleware } from "../middleware/access.middleware.js";
+import { UserAccessStrategy } from "../middleware/access-strategies/user.access-strategy.js";
 
 export const UserRouter = Router();
 
@@ -24,12 +26,20 @@ UserRouter.get(
   "/:userId",
   AuthMiddleware({ passOnRedirect: true }),
   MeMiddleware({ targetReqParam: "userId" }),
+  AccessMiddleware(new UserAccessStrategy(), {
+    property: "userId",
+    propertyLocation: "params",
+  }),
   controller.getOne.bind(controller)
 );
 UserRouter.put(
   "/:userId",
   AuthMiddleware({ passOnRedirect: true }),
   MeMiddleware({ targetReqParam: "userId" }),
+  AccessMiddleware(new UserAccessStrategy(), {
+    property: "userId",
+    propertyLocation: "params",
+  }),
   DtoValidationMiddleware(UpdateUserDto, "body"),
   controller.update.bind(controller)
 );
@@ -37,5 +47,9 @@ UserRouter.delete(
   "/:userId",
   AuthMiddleware({ passOnRedirect: true }),
   MeMiddleware({ targetReqParam: "userId" }),
+  AccessMiddleware(new UserAccessStrategy(), {
+    property: "userId",
+    propertyLocation: "params",
+  }),
   controller.delete.bind(controller)
 );
