bd727706ca2c736f61c1ea90fe8b7a8ff9ff8795
Denys Bolshakov <deni.bolshakov.ua@gmail.com>
2024-02-18 16:00:47 +0200

    get all students filters fix

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { AccessMiddleware } from "../middleware/access.middleware.js";
 import { StudentAccessStrategy } from "../middleware/access-strategies/student.access-strategy.js";
 import { FilterStudentAssignmentDto } from "../dto/assignment/filter-student-assignment.dto.js";
-import { FilterTeacherDto } from "../dto/teacher/filter-teacher.dto.js";
 import { AuthMiddleware } from "../middleware/auth.middleware.js";
 import { DtoValidationMiddleware } from "../middleware/dto-validation.middleware.js";
 import { container } from "tsyringe";
@@ -16,11 +15,7 @@ const controller = container.resolve<StudentController>("student-controller");
 
 StudentRouter.post("/", AuthMiddleware(), controller.create.bind(controller));
 
-StudentRouter.get(
-  "/",
-  DtoValidationMiddleware(FilterTeacherDto, "query"),
-  controller.getMany.bind(controller)
-);
+StudentRouter.get("/", controller.getMany.bind(controller));
 StudentRouter.get("/:studentId", controller.getOne.bind(controller));
 StudentRouter.get(
   "/:studentId/courses",
